819de460df44e36634197cd29d250c2a0a2f647b
bhaskarreddy728 <bhaskarreddy728@gmail.com>
2023-02-02 22:49:44 +0530

    Assignment using for loop using string length method

---
+++ src/test/java/day5/Loop1.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Loop1 {
+    public static void main(String[]args){
+        String names[] = {"Bhaskar","Ujwal","Amrutha","Sachi","Sanpa"};
+        for(int i = 0;i< names.length;i++)
+        {
+        System.out.println(names[i]);
+        }
+    }
+}

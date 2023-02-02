a536c2e1468d1c23606749928678f41f52dfe5e8
bhaskarreddy728 <bhaskarreddy728@gmail.com>
2023-02-02 23:04:28 +0530

    Assignment using for each loop

---
+++ src/test/java/day5/Loop2.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class Loop2 {
+    public static void main(String[] args) {
+        String names[] = {"Bhaskar", "Ujwal", "Amrutha", "Sachi", "Sanpa"};
+        for (String str : names) {
+            System.out.println(str);
+        }
+    }
+}
